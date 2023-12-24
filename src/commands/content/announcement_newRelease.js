39df90f0058e000046bd619dcff8ed337486345a
Álvaro Renan <119453981+YoruTheWizard@users.noreply.github.com>
2023-12-24 20:42:01 -0300

    Renaming "new chapter" slash command to "new release"

---
+++ src/commands/content/announcement_newRelease.js
@@ -0,0 +1,107 @@
+const { ApplicationCommandOptionType, Client, Interaction, EmbedBuilder } = require("discord.js");
+const { testServer } = require('../../config.json');
+const scanTitles = require('../../json/scanTitles.json');
+// const newChapterEmbed = require('../../embeds/newChapterEmbed.json');
+
+module.exports = {
+  devOnly: false,
+  testOnly: false,
+  deleted: false,
+
+  name: 'novolancamento',
+  description: 'Manda um anúncio para todos no servidor sobre um novo capítulo.',
+  options: [
+    {
+      name: 'obra',
+      description: 'Nome da obra',
+      type: ApplicationCommandOptionType.String,
+      choices: [],
+      required: true
+    },
+    {
+      name: 'tipo',
+      description: 'Tipo de lançamento',
+      type: ApplicationCommandOptionType.String,
+      choices: [
+        {
+          name: 'Capítulo',
+          value: 'capítulo'
+        },
+        {
+          name: 'Volume',
+          value: 'volume'
+        }
+      ],
+      required: true
+    },
+    {
+      name: 'numero',
+      description: 'Número do capítulo / volume',
+      type: ApplicationCommandOptionType.Number,
+      required: true
+    },
+    {
+      name: 'link',
+      description: 'Link para ler o capítulo',
+      type: ApplicationCommandOptionType.String,
+      required: true
+    },
+    {
+      name: 'descricao',
+      description: 'Descrição do capítulo',
+      type: ApplicationCommandOptionType.String
+    }
+  ],
+  // permissionsRequired: PermissionFlagsBits.Administrator,
+  // botPermissions: PermissionFlagsBits.Administrator,
+
+  /**
+   * 
+   * @param {Client} client 
+   * @param {Interaction} interaction 
+   */
+  callback: async (client, interaction) => {
+    if (interaction.guild.id !== testServer)
+      if (!interaction.member.roles.cache.some(role => role.id === '1123264880800182352')) interaction.reply({
+        content: 'Apenas membros da staff podem usar este comando!',
+        ephemeral: true
+      });
+    const titleName = interaction.options.get('obra').value,
+      type = interaction.options.get('tipo').value,
+      number = interaction.options.get('numero').value,
+      titleDescription = interaction.options.get('descricao')?.value,
+      titleLinks = interaction.options.get('link').value.split(', ');
+
+    let links = '- '.concat(titleLinks.join('\n- '));
+
+    let titleObj;
+    for (let title of scanTitles)
+      if (title.id === titleName) titleObj = title;
+
+    const newChapterEmbed = new EmbedBuilder()
+      .setColor(titleObj.color)
+      .setAuthor({ name: titleObj.longNameJP || titleObj.longName })
+      .setTitle(`Novo ${type} de ${titleObj.name}!`)
+      .setDescription(
+        `O ${type} **${number}** já está disponível! Venha ver!<${titleObj.emoji || ':tada:'}>`
+      );
+
+    if (titleDescription)
+      newChapterEmbed.addFields({ name: 'Descrição', value: titleDescription });
+
+    newChapterEmbed.addFields({ name: 'Links', value: links });
+
+    // newChapterEmbed.color = parseInt(titleObj.color);
+    // newChapterEmbed.author.name = newChapterEmbed.author.name
+    //   .replace('{title}', titleObj.longNameJP || titleObj.longName);
+    // newChapterEmbed.title = newChapterEmbed.title.replace('{title}', titleObj.name);
+    // newChapterEmbed.description = newChapterEmbed.description
+    //   .replace('{chapter}', titleChapter)
+    //   .replace('{emoji}', titleObj.emoji ? titleObj.emoji : ':tada:');
+    // if (titleDescription)
+    //   newChapterEmbed.fields[0].value = newChapterEmbed.fields[0].value.replace('{description}', titleDescription);
+    // newChapterEmbed.fields[1].value = titleURL;
+
+    await interaction.channel.send({ content: `<@&${titleObj.fanRole}>`, embeds: [newChapterEmbed] });
+  }
+};
