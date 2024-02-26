31d2a6e787f242ad33da8c108a06ef843354cb37
Álvaro Renan <119453981+YoruTheWizard@users.noreply.github.com>
2024-02-26 20:24:23 -0300

    Updating 'scanbloopers' slash command

--- src/commands/content/scanBloopers.js
+++ src/commands/content/scanBloopers.js
@@ -1,9 +1,19 @@
 const { SlashCommandBuilder, Client, Interaction } = require('discord.js');
+const { authors } = require('../../json/scanBloopers.json');
+
+const setAuthorOption = opt => {
+  opt.setName('autor')
+    .setDescription('O autor da pérola');
+  for (let author of authors)
+    opt.addChoices({ name: author, value: author });
+  return opt;
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('perolasdoservidor')
-    .setDescription('Manda aleatoriamente uma pérola do servidor'),
+    .setDescription('Manda aleatoriamente uma pérola do servidor')
+    .addStringOption(opt => setAuthorOption(opt)),
 
   /**
    * 
@@ -13,8 +23,15 @@ module.exports = {
    * }} param0
    */
   run: async ({ interaction, client }) => {
-    const bloopers = require('../../json/scanBloopers.json');
-    const rand = parseInt(Math.random() * bloopers.length);
-    interaction.reply(bloopers[rand]);
+    const authorFilter = interaction.options.get('autor')?.value;
+
+    let rand, blooper;
+    const { bloopers } = require('../../json/scanBloopers.json');
+    do {
+      rand = parseInt(Math.random() * bloopers.length);
+      blooper = bloopers[rand];
+    } while (authorFilter && blooper.author !== authorFilter);
+
+    interaction.reply(`"${blooper.message}" ~ ${blooper.authorName}. ${blooper.date}`);
   }
 };
